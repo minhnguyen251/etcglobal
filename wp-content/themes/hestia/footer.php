043d926456fd8771bb8bd9f4df9f26d99988b7e1
Minh NGUYEN <minh.n@socialshaker.com>
2018-02-15 14:37:03 +0100

    15/02

--- wp-content/themes/hestia/footer.php
+++ wp-content/themes/hestia/footer.php
@@ -11,6 +11,15 @@
 
 		</div>
 	</div>
+<section class="contact-us overlay">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <h2 class="hestia-title">Liên hệ với ETC Global</h2>
+                <div class="hestia-description"><div style="color: white;font-size: 16px"><p class="font_0"><span>Add: 66 Av. des Champs-Élysées, 75008 Paris</span></p><p class="font_0"><span>E-mail:&nbsp;</span><span><a href="mailto:info@etc-global.fr" target="_self" style="color: white">info@etc-global.fr</a></span><span>​</span></p><p class="font_0"><span>Hotline:&nbsp;</span><span>+84 9 89 88 56 65</span></p><p class="font_0"><span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;+33 6 99 77 67 89&nbsp;(Viber/Tango/Zalo/WhatsApp/...)</span></p></div></div>					</div>
+        </div>
+    </div>
+</section>
 <?php wp_footer(); ?>
 </body>
 </html>
